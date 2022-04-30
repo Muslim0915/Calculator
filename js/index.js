9d4e39f5b02282b644485625ab0977765e87912b
Muslim0915 <muslimpro0915@gmail.com>
2022-04-30 05:33:52 +0500

    feat: add js codes

---
+++ js/index.js
@@ -0,0 +1,108 @@
+"use strict";
+
+    const input = document.getElementById('input');
+    const number = document.querySelectorAll('.numbers div');
+    const operator = document.querySelectorAll('.operators div');
+    const result = document.getElementById('result');
+    const clear = document.getElementById('clear');
+    const deleteButton = document.querySelector('.delete');
+
+    let resultDisplayed = false;
+    let newString;
+    let currentString;
+
+for (let i = 0; i < number.length; i++) {
+    number[i].addEventListener("click", function(e) {
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
+
+        if (resultDisplayed === false) {
+            input.innerHTML += e.target.innerHTML;
+        } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+            resultDisplayed = false;
+            input.innerHTML += e.target.innerHTML;
+        } else {
+            resultDisplayed = false;
+            input.innerHTML = "";
+            input.innerHTML += e.target.innerHTML;
+        }
+
+    });
+}
+
+for (let i = 0; i < operator.length; i++) {
+    operator[i].addEventListener("click", function(e) {
+
+        currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
+
+        if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+             newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+            input.innerHTML = newString;
+        } else if (currentString.length == 0) {
+            console.log("enter a number first");
+        } else {
+            input.innerHTML += e.target.innerHTML;
+        }
+
+    });
+}
+
+result.addEventListener("click", function() {
+
+    let inputString = input.innerHTML;
+
+    let numbers = inputString.split(/\+|\-|\×|\÷/g);
+
+    let operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+    console.log(inputString);
+    console.log(operators);
+    console.log(numbers);
+    console.log("----------------------------");
+
+
+    let divide = operators.indexOf("÷");
+    while (divide != -1) {
+        numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+        operators.splice(divide, 1);
+        divide = operators.indexOf("÷");
+    }
+
+    let multiply = operators.indexOf("×");
+    while (multiply != -1) {
+        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+        operators.splice(multiply, 1);
+        multiply = operators.indexOf("×");
+    }
+
+    let subtract = operators.indexOf("-");
+    while (subtract != -1) {
+        numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+        operators.splice(subtract, 1);
+        subtract = operators.indexOf("-");
+    }
+
+    let add = operators.indexOf("+");
+    while (add != -1) {
+        numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+        operators.splice(add, 1);
+        add = operators.indexOf("+");
+    }
+
+    input.innerHTML = numbers[0];
+
+    resultDisplayed = true;
+});
+
+clear.addEventListener("click", function() {
+    input.innerHTML = "";
+});
+deleteButton.addEventListener('click', function (e) {
+    if (input.innerText.length !== 0) {
+        input.innerHTML = input.innerHTML.slice(0, -1);
+        document.querySelector('[name="decimal"]').disabled = false;
+    } else {
+    }
+});
+
